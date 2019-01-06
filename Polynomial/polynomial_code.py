b5ebfc551be548b33413b380478b2ea6f414a158
Ehsan HajYasini <esihaj@gmail.com>
2019-01-06 21:06:42 +0530

    Fix polynomial code

--- Polynomial/polynomial_code.py
+++ Polynomial/polynomial_code.py
@@ -3,8 +3,11 @@ import numpy as np
 import random
 import threading
 import time
-from scipy.interpolate import lagrange
+from scipy import interpolate
 import logging
+
+NP_DATA_TYPE = np.float64
+MPI_DATA_TYPE = MPI.DOUBLE
 
 def loop():
     t = time.time()
@@ -23,8 +26,8 @@ class PolynomialCoder:
         self.straggling = False
         self.comm = comm
         self.N = N
-        self.A = A
-        self.B = B
+        self.A = A.astype(NP_DATA_TYPE)
+        self.B = B.astype(NP_DATA_TYPE)
         self.s = A.shape[0]
         self.r = A.shape[1]
         self.t = B.shape[1]
@@ -34,6 +37,7 @@ class PolynomialCoder:
         logging.debug("var:\n" + str(self.var))
         # self.zero_padding_matrices()
         self.F = F
+        self.coeffs = None
 
     def zero_padding_matrices(self):
         s = self.s
@@ -55,7 +59,6 @@ class PolynomialCoder:
         self.t = new_t
 
     def data_send(self):
-
         comm = self.comm
         A = self.A
         B = self.B
@@ -64,6 +67,10 @@ class PolynomialCoder:
         n = self.n
         m = self.m
         N = self.N
+
+        spec_dict = [self.r, self.t, self.s, F, n, m, N]
+        # logging.debug("r,t,s,F,n,m,N\n" + str(spec_dict))
+        comm.bcast(spec_dict)
 
         # Decide and broadcast chosen straggler
         straggler = random.randint(1, N)
@@ -77,20 +84,19 @@ class PolynomialCoder:
         # Encode the matrices
         Aenc = [sum([Ap[j] * (pow(var[i], j, F)) for j in range(m)]) % F for i in range(N)]
         Benc = [sum([Bp[j] * (pow(var[i], j * m, F)) for j in range(n)]) % F for i in range(N)]
-        Benc = np.asarray(Benc, dtype=np.int)
-        # Aenc = [sum([Ap[j] * (pow(var[i], j)) for j in range(m)]) for i in range(N)]
-        # Benc = [sum([Bp[j] * (pow(var[i], j * m)) for j in range(n)])for i in range(N)]
 
-        logging.debug("Aenc:\n" + str(Aenc))
-        logging.debug("Benc:\n" + str(Benc))
+        logging.debug("Aenc:" + str(Aenc[0].dtype) + "\n" + str(Aenc))
+        logging.debug("Benc:" + str(Benc[0].dtype) + "\n" + str(Benc))
 
         # Start requests to send
         request_A = [None] * N
         request_B = [None] * N
         self.bp_start = time.time()
+        logging.debug("A[i]" + str(Aenc[i].shape) + ", dtype=" + str(Aenc[i].dtype))
+        logging.debug("B[i]" + str(Benc[i].shape) + ", dtype=" + str(Benc[i].dtype))
         for i in range(N):
-            request_A[i] = comm.Isend([Aenc[i], MPI.INT], dest=i + 1, tag=15)
-            request_B[i] = comm.Isend([Benc[i], MPI.INT], dest=i + 1, tag=29)
+            request_A[i] = comm.Isend([Aenc[i], MPI_DATA_TYPE], dest=i + 1, tag=15)
+            request_B[i] = comm.Isend([Benc[i], MPI_DATA_TYPE], dest=i + 1, tag=29)
         MPI.Request.Waitall(request_A)
         MPI.Request.Waitall(request_B)
 
@@ -115,86 +121,97 @@ class PolynomialCoder:
         # Initialize return dictionary
         return_dict = []
         for i in range(N):
-            return_dict.append(np.zeros((int(r / m), int(t / n)), dtype=np.int_))
+            return_dict.append(np.zeros((int(r / m), int(t / n)), dtype=NP_DATA_TYPE))
 
         # Start requests to receive
         request_C = [None] * N
         for i in range(N):
-            request_C[i] = comm.Irecv([return_dict[i], MPI.INT], source=i + 1, tag=42)
+            request_C[i] = comm.Irecv([return_dict[i], MPI_DATA_TYPE], source=i + 1, tag=42)
 
         return_C = [None] * N
-        list = []
+        recv_index = []
         # Wait for the mn fastest workers
         for i in range(m * n):
             j = MPI.Request.Waitany(request_C)
-            list.append(j)
+            recv_index.append(j)
             return_C[j] = return_dict[j]
 
         self.bp_received = time.time()
         logging.info("Time spent waiting for %d workers %s is: %f" % (
-            m * n, ",".join(map(str, [x + 1 for x in list])), (self.bp_received - self.bp_sent)))
+            m * n, ",".join(map(str, [x + 1 for x in recv_index])), (self.bp_received - self.bp_sent)))
 
         logging.debug("return C: " + str(return_C))
+
+        self.coeffs = self.calculate_C(return_C, recv_index)
+        self.bp_done = time.time()
+        logging.info("Time spent decoding is: %f" % (self.bp_done - self.bp_received))
+
+    def calculate_base_indices(self):
+        r, s, t, m, n = self.r, self.s, self.t, self.m, self.n
         base_indices = []
         for j in range(n):
             for i in range(m):
-                base_indices.append([i*int(r/m), j*int(t/n)])
+                base_indices.append([i * int(r / m), j * int(t / n)])
         base_indices = tuple(reversed(base_indices))
         logging.debug("base_indices:\n" + str(base_indices))
-        # Lagrange polynomial interpolation
-        # coeffs = np.zeros((int(r / m), int(t / n), m * n))
+        return base_indices
+
+    def calculate_C(self, return_C, recv_index):
+        """
+        :param return_C: Ci calculated by workers
+        :return: final C
+        """
+        r, s, t, m, n = self.r, self.s, self.t, self.m, self.n
+
+        base_indices = self.calculate_base_indices()
+
         # list is 0 based but our workers, and Aenc, Benc matrices are 1 based
         # so we need to convert the list
-        recv_var = tuple(map(lambda x: x+1, list))
-        coeffs = np.zeros((r, t))
+        recv_var = tuple(map(lambda x: x + 1, recv_index))
+        coeffs = np.zeros((r, t), dtype=NP_DATA_TYPE)
+        logging.info("looping for %d" % (int(r / m) * int(t / n)))
         for i in range(int(r / m)):
             for j in range(int(t / n)):
                 f_z = []
                 for k in range(m * n):
-                    f_z.append(return_C[list[k]][i][j])
-                lagrange_interpolate = lagrange(recv_var, f_z)
-                logging.debug("list: %s,\nf_z: %s,\nlag: %s" %
-                              tuple(map(lambda x: str(x), [list, f_z, lagrange_interpolate])))
+                    f_z.append(return_C[recv_index[k]][i][j])
+                lagrange_interpolate = interpolate.lagrange(recv_var, f_z)
                 for index, lag_coef in enumerate(lagrange_interpolate):
-                    coeffs[i+base_indices[index][0]][j+base_indices[index][1]] = lag_coef
-        logging.debug("coeffs: " + str(coeffs.shape))
+                    coeffs[i + base_indices[index][0]][j + base_indices[index][1]] = lag_coef
 
-        logging.info("coeffs:\n" + str(coeffs))
+        # logging.debug("coeffs: " + str(coeffs.shape))
+        # logging.info("coeffs:\n" + str(coeffs))
+        return coeffs
 
-        # TODO: Create C by combibing C_tildes
+    @staticmethod
+    def mapper(comm, barrier_enabled, straggling_enabled):
+        spec_dict = comm.bcast(None)
+        # spec_dict = [3073, 10, 500, 2125991977, 1, 7, 8]
+        logging.debug("spec_dict " + str(spec_dict))
+        r, t, s, F, n, m, N = spec_dict
 
-        self.bp_done = time.time()
-        logging.info("Time spent decoding is: %f" % (self.bp_done - self.bp_received))
-
-    def mapper(self):
-
-        comm = self.comm
-        F = self.F
-        s = self.s
-        n = self.n
-        m = self.m
-        r = self.r
-        t = self.t
         # Receive straggler information from the master
         straggler = comm.recv(source=0, tag=7)
 
-
         # Receive split input matrices from the master
-        Ai = np.empty_like(np.matrix([[0] * int(r / m) for i in range(s)]))
-        Bi = np.empty_like(np.matrix([[0] * int(t / n) for i in range(s)]))
+        Ai = np.empty_like(np.matrix([[0] * int(r / m) for i in range(s)]), dtype=NP_DATA_TYPE)
+        Bi = np.empty_like(np.matrix([[0] * int(t / n) for i in range(s)]), dtype=NP_DATA_TYPE)
         receive_A = comm.Irecv(Ai, source=0, tag=15)
         receive_B = comm.Irecv(Bi, source=0, tag=29)
+
+        logging.debug("Ai[receiver] " + str(Ai.shape) + ", dtype=" + str(Ai.dtype))
+        logging.debug("Bi[receiver] " + str(Bi.shape) + ", dtype=" + str(Bi.dtype))
 
         receive_A.wait()
         receive_B.wait()
 
-        if self.barrier:
+        if barrier_enabled:
             comm.Barrier()
 
-        self.wbp_received = time.time()
+        wbp_received = time.time()
 
         # Start a separate thread to mimic background computation tasks if this is a straggler
-        if self.straggling:
+        if straggling_enabled:
             if straggler == comm.rank:
                 t = threading.Thread(target=loop)
                 t.start()
@@ -205,16 +222,13 @@ class PolynomialCoder:
                       + "\nC:\n" + str(Ci))
 
         # print("Ci["+ str(comm.Get_rank()) +"]", Ci )
-        self.wbp_done = time.time()
-        logging.info("Worker %d computing takes: %f\n" % (comm.Get_rank(), self.wbp_done - self.wbp_received))
+        wbp_done = time.time()
+        logging.info("Worker %d computing takes: %f\n" % (comm.Get_rank(), wbp_done - wbp_received))
 
-        sC = comm.Isend(Ci, dest=0, tag=42)
+        sC = comm.Isend([Ci, MPI_DATA_TYPE], dest=0, tag=42)
         sC.Wait()
 
     def polynomial_code(self):
-        comm = self.comm
-        if comm.Get_rank()  == 0:
+        if self.comm.Get_rank() == 0:
             self.data_send()
             self.reducer()
-        else:
-            self.mapper()
